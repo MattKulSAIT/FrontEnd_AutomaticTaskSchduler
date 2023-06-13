5e253f2864123baf5ea06d1dfef6022e1223ecd0
Miguel Mulingbayan <122918015+MiguelMwithTheM@users.noreply.github.com>
2023-06-13 09:38:24 -0600

    Deleted components

--- src/components/components/Layout_Display/pageTitle.js
+++
@@ -1,3 +0,0 @@
-const name = banan;
-
-const titlePageName = <h1>{name}</h1>;
--- src/components/components/test.js
+++
@@ -1,9 +0,0 @@
-const banan = (
-    <h1>Banan</h1>
-);
-
-export default function doBanan() {
-    return (
-        <h1>Hello</h1>
-    );
-}
