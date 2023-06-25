2b551100a9ee9e2ea24a42e8f12799585c84857b
Miguel Mulingbayan <122918015+MiguelMwithTheM@users.noreply.github.com>
2023-06-25 17:16:39 -0600

    Add files via upload

---
+++ src/pages/users/resourceSpecific/menu_Resource/menu_Resource.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './menu_Resource.css'
+
+import ResourceMenu from '../../../../components/resourceMenu'
+
+function MenuResource() {
+    return (
+        <ResourceMenu/>
+    );
+}
+
+export default MenuResource;
