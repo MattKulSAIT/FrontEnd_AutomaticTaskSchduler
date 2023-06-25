f39d1c10f155ea07139f31c3235112d946ee1504
Miguel Mulingbayan <122918015+MiguelMwithTheM@users.noreply.github.com>
2023-06-25 17:17:01 -0600

    Add files via upload

---
+++ src/pages/users/adminSpecific/menu_Admin/menu_Admin.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './menu_Admin.css'
+
+import AdminMenu from "../../../../components/adminMenu";
+
+function MenuResource() {
+    return (
+        <AdminMenu/>
+    );
+}
+
+export default MenuResource;
