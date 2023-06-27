9fd7f69b53beefb2ab8cfb5e265db4989eb8f1a6
Miguel Mulingbayan <122918015+MiguelMwithTheM@users.noreply.github.com>
2023-06-27 10:32:12 -0600

    Add files via upload

--- src/components/createResource.js
+++ src/components/createResource.js
@@ -104,40 +104,60 @@ const CreateResource = () => {
                 <form method='GET' onSubmit={createResource}>
                     <div className='mainPage'> 
                         <hr align="left"/>
-                        <div className='rescFirstNameDiv'>
-                            <h3>Change Eestimated Time:</h3>
-                            <input type='text' placeholder='Ex. John' name="rescFirstName"/>
+                        <div className='rescDetails'>
+                            <div className='rescFirstNameDiv'>
+                                <h3>First Name</h3>
+                                <input type='text' placeholder='Ex. John' name="rescFirstName"/>
+                            </div>
+                            <div className='rescLastNameDiv'>
+                                <h3>Last Name</h3>
+                                <input type='text' placeholder='Ex. Doe' name="rescLastName"/>
+                            </div>
+                            <div className='rescPhoneDiv'>
+                                <h3>Phone</h3>
+                                <input type='phone' placeholder='Ex. 123-123-1234' name="rescPhone"/>
+                            </div>
+                            <div className='rescEmailDiv'>
+                                <h3>Email</h3>
+                                <input type='email' placeholder='Ex. johnd@work.com' name="rescEmail"/>
+                            </div>
+                            <div className='passDiv'>
+                                <h3>Set Password</h3>
+                                <input type='text' placeholder='Ex. ***********' name="rescPassword"/>
+                            </div>
                         </div>
-                        <div className='rescLastNameDiv'>
-                            <h3>Change Eestimated Time:</h3>
-                            <input type='text' placeholder='Ex. Doe' name="rescLastName"/>
+                        <hr align="left"/>
+                        <div className='taskSkillsDiv'>
+                            <table align='left'>
+                                <tr>
+                                    <td><h3>Skills:</h3></td>
+                                </tr>
+                                <tr>
+                                    <td className='tdButton'><h3><input type='checkbox' id='helpdesk' name='helpdesk' className='skillsButton'/>Help Desk Support</h3></td>
+                                    <td className='tdButton'><h3><input type='checkbox' id='database' name='database' className='skillsButton'/>Database Support</h3></td>
+                                </tr>
+                                <tr>
+                                    <td className='tdButton'><h3><input type='checkbox' id='network' name='network' className='skillsButton'/>Database Support</h3></td>
+                                    <td className='tdButton'><h3><input type='checkbox' id='telecomm' name='telecomm' className='skillsButton'/>Database Support</h3></td>
+                                </tr>
+                            </table>                    
                         </div>
-                        <div className='rescPhoneDiv'>
-                            <h3>Change Eestimated Time:</h3>
-                            <input type='phone' placeholder='Ex. 123-123-1234' name="rescPhone"/>
-                        </div>
-                        <div className='rescEmailDiv'>
-                            <h3>Change Eestimated Time:</h3>
-                            <input type='email' placeholder='Ex. johnd@work.com' name="rescEmail"/>
-                        </div>
-                        <div className='taskSkills'>
-                            <h3>Skills:</h3>
-                                <input type='checkbox' id='helpdesk' name='helpdesk' value="0"/>
-                                    <label for="helpdesk">Help Desk Support</label>
-                                <input type='checkbox' id='database' name='database' value="1"/>
-                                    <label for="database">Database Support</label>
-                                <input type='checkbox' id='network' name='network' value="2"/>
-                                    <label for="network">Network Support</label>
-                                <input type='checkbox' id='telecomm' name='telecomm' value="3"/>
-                                    <label for="telecomm">Mobile Telecommunications Support</label>
-                        </div>
-                        <div className='taskStatus'>
-                            <h3>Change Task Status:</h3>
-                                <input type='radio' id='rescRole' name='roles' value="0"/>
-                                    <label for="rescRole">Resource</label>
-                                <input type='radio' id='adminRole' name='roles' value="1"/>
-                                    <label for="adminRole">Admin</label>
-                        </div>
+                        <div className='rescRoleDiv'>
+                            <table align='left'>
+                                <tr>
+                                    <td><h3>Change Task Type:</h3></td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                    <select name="selectTaskType" id="taskStatus">
+                                        <option value="" disabled selected>- Select Status -</option>
+                                        <option value="0">Help Desk Support</option>
+                                        <option value="1">Database Support</option>
+                                    </select> 
+                                    </td>
+                                </tr>
+                            </table>                                 
+                        </div> 
                         <div className='editButtons'>
                             <button id='createResource' type="submit">Create</button>
                             <button id='exitEdit' type="button" onClick={exitEdit}>Exit</button>
@@ -153,3 +173,19 @@ const CreateResource = () => {
 }
 
 export default CreateResource;
+
+/*
+                            <table align='left'>
+                                <tr>
+                                    <td><h3>Roles:</h3></td>
+                                </tr>
+                                <tr>
+                                    <td className='tdButton'><input type='radio' id='rescRole' name='roles' className='roleButton' value="0"/></td>
+                                    <td className='tdTitle'><h3>Resource</h3></td>
+                                </tr>
+                                <tr>
+                                    <td className='tdButton'><input type='radio' id='adminRole' name='roles' className='roleButton' value="1"/></td>
+                                    <td className='tdTitle'><h3>Admin</h3></td>
+                                </tr>                          
+                            </table>    
+*/
