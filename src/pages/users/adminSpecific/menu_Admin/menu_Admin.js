85f0839bb7f7cc16688469887eb2eddd8f6770b5
Miguel Mulingbayan <122918015+MiguelMwithTheM@users.noreply.github.com>
2023-06-27 12:52:32 -0600

    Menu Hotfix

--- src/pages/users/adminSpecific/menu_Admin/menu_Admin.js
+++ src/pages/users/adminSpecific/menu_Admin/menu_Admin.js
@@ -1,11 +1,19 @@
 import React from "react";
 import './menu_Admin.css'
+import PageHeader from "../../../../components/pageHeader";
 
 import AdminMenu from "../../../../components/adminMenu";
 
 function MenuResource() {
     return (
-        <AdminMenu/>
+        <div>
+            <div className="header">
+                //PagHeader
+            </div>
+            <div className="body">
+                <AdminMenu/>
+            </div>   
+        </div>
     );
 }
 
