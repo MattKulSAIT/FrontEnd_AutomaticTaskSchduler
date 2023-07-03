7ee0354a467730ad326f79da7d493335b8978ea7
Miguel Mulingbayan <122918015+MiguelMwithTheM@users.noreply.github.com>
2023-07-02 22:23:41 -0600

    Add files via upload

---
+++ src/pages/users/adminSpecific/resourceEdit_Admin/resourceEdit_Admin.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './resourceEdit_Admin.css'
+//import PageHeader from '../../../../components/pageHeader';
+//import PageTitle from '../../../../components/pageTitle';
+import RescEditAdmin from '../../../../components/editViews/resourceEdit';
+
+function ResourceEditAdmin() {
+    return (
+        <div className="ResourceEditAdmin">
+            <div className="components">
+                <div className="headers">
+                //PageHeader
+                //PageTitle
+                </div>
+                <div className="body">
+                    <RescEditAdmin/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ResourceEditAdmin;
