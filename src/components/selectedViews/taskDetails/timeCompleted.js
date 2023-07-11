5b9fe9b06c9baef264ad400063b581f55cdd066c
Miguel Mulingbayan <122918015+MiguelMwithTheM@users.noreply.github.com>
2023-07-11 10:00:33 -0600

    Add files via upload

--- src/components/selectedViews/taskDetails/timeCompleted.js
+++ src/components/selectedViews/taskDetails/timeCompleted.js
@@ -4,36 +4,20 @@ import { useParams } from 'react-router-dom';
 function CompDetails() {
 
     /*
-    const [compTime, setCompTime] = useState("tempECTtime");
-    const { id } = useParams();
-
-    useEffect(() => {
-        fetchData();
-      }, []);
-
-      const fetchData = async () => {
-        try {
-          const response = await fetch(`http://localhost:8080/taskView/${id}`); //it used id because thats what was useing the App.js
-          if (response.ok) {
-            const data = await response.json();
-            setEctTime(data.timeToComplete);
-          } else {
-            throw new Error('Failed to fetch data');
-          }
-        } catch (error) {
-          console.error(error);
-        }
-      };
+    const [taskCreation, setTaskCreation] = useState("tempTaskCreation");
 
       */
+
 
     return(
         <div className="compTimeDiv">
             <table className="compTimeTable">
                 <tr>
+                    <th>Creation Date:</th>
                     <th>Completion Date:</th>
                 </tr>
                 <tr>
+                    <td id='createTime'>{createTime}</td>
                     <td id="compTime">{compTime}</td>
                 </tr>
             </table>
