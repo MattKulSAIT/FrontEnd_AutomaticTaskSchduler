35e92fda962f5c4048fc5654e328a51b3c334e52
Miguel Mulingbayan <122918015+MiguelMwithTheM@users.noreply.github.com>
2023-07-10 18:55:49 -0600

    Add files via upload

---
+++ src/pages/users/resourceSpecific/scheduleEdit_Resource/scheduleEdit_Resource.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import './scheduleEdit_Resource.css'
+
+import PageHeader from'../../../../components/pageHeader';
+import PageTitle from '../../../../components/pageTitle';
+import EditCalendar from '../../../../components/editScheduleView/editCalendar';
+import EditHours from '../../../../components/editScheduleView/editHours';
+import DisplayHours from '../../../../components/editScheduleView/displayHours';
+
+function ScheduleEditResource() {
+
+    const currPage = "Change Schedule";
+    const backButtonLink = `http://localhost:3000/menu_Resource`;
+
+
+    return (
+        <div className="scheduleEditResource">
+            <div className="components">
+                <div className='headers'>
+                    <PageHeader/>
+                    <PageTitle currPage={currPage} backButtonLink={backButtonLink}/>
+                </div>
+                <div className='body'>
+                    <div className='leftSide'>
+                        //EditCalendar
+                        <EditHours/>
+                    </div>
+                    <div className='rightSide'>
+                        <DisplayHours/>
+                    </div> 
+                </div>
+            </div>
+        </div>
+    );
+}
+  
+export default ScheduleEditResource;
